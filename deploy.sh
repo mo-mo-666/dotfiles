f91fa9429e63571c5d3430aeff5dba064fb5ec04
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2020-04-17 04:40:10 +0900

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Link dotfiles under ~.
+
+DOT_DIRECTORY="${HOME}/dotfiles"
+MODE=${1}
+MODE=${MODE:="normal"}
+force_mode="force"
+
+
+# OS recognition
+if [ "$(uname)" == 'Darwin' ]; then
+    BASE_OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    if [[ "$(uname -r)" == *Microsoft* ]]; then
+        BASE_OS='WSL'
+    else
+        BASE_OS='Linux'
+    fi
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+echo "This is ${BASE_OS}."
+
+cd $DOT_DIRECTORY
+for f in .??*
+do
+    if [ "$MODE" = "$force_mode" -o ! -e "${HOME}/${f}" ] ; then
+        [ "$f" = ".git" ] && continue
+        [ "$f" = ".gitignore" ] && continue
+        [ "$f" = ".config" ] && continue
+
+        ln -snfv "$DOT_DIRECTORY/$f" "$HOME/$f"
+    fi
+done
+if [ "$MODE" = "$force_mode" -o ! -e "${HOME}/.config/fish/config.fish" ] ; then
+    ln -snfv "$DOT_DIRECTORY/.config/fish/config.fish" "$HOME/.config/fish/config.fish"
+fi
