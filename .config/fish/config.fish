163177aed9c107cdd0e4d9cb4026d55f98ec4ae9
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2020-04-18 04:49:11 +0900

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
 # ----------
 # bobthefish config
 # ----------
-# set -g fish_prompt_pwd_dir_length 0  # ディレクトリ省略しない
+set -g fish_prompt_pwd_dir_length 0  # ディレクトリ省略しない
 set -g theme_newline_cursor no  # プロンプトを改行した先に設ける
 set -g theme_display_git_master_branch yes  # git の branch 名を表示
-set -g theme_color_scheme default
+# set -g theme_color_scheme dark
 set -g theme_display_date no  # 時刻を表示しないように設定
 set -g theme_display_cmd_duration no # コマンド実行時間の非表示
 
@@ -23,8 +23,8 @@ alias gmg='git merge'
 alias gps='git push'
 alias gst='git status'
 
-alias rm='rm -i'
-alias mv='mv -i'
-alias cp='cp -i'
+# alias rm='rm -i'
+# alias mv='mv -i'
+# alias cp='cp -i'
 alias ..='cd ..'
 alias ...='cd ../..'
