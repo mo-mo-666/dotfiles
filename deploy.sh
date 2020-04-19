96673a93ff5b90d00537b35e251683aa77ba6fff
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2020-04-20 02:41:43 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,49 +1,45 @@
 #!/bin/bash
 # -----------------------
 # Link dotfiles under ~.
+# execute './deploy.sh' for normal node and './deploy.sh force' for force mode.
 # -----------------------
 
+# Stop all if some error or undefined variable ocurred.
+set -eu
+. ./common.sh
 
-DOT_DIRECTORY="${HOME}/dotfiles/dotfiles"
 MODE=${1}
 MODE=${MODE:="normal"}
 force_mode="force"
 
-
-# OS recognition
-if [ "$(uname)" == 'Darwin' ]; then
-    BASE_OS='Mac'
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    if [[ "$(uname -r)" == *Microsoft* ]]; then
-        BASE_OS='WSL'
+link_file()
+{
+    if [ -f "$2" ] && [ ! -L "$2" ] ; then
+        if [ "$MODE" = "$force_mode" ] ; then
+            mkdir -p $(dirname $2)
+            mv -fv $2 $3
+            ln -snfv $1 $2
+        fi
     else
-        BASE_OS='Linux'
+        ln -snfv $1 $2
     fi
-else
-    echo "Your platform ($(uname -a)) is not supported."
-    exit 1
-fi
-echo "This is ${BASE_OS}."
+}
 
-cd $DOT_DIRECTORY
-for f in .??*
+
+cd $DOT_DIR
+for f in `find -type f`
 do
-    if [ "$MODE" = "$force_mode" -o ! -e "${HOME}/${f}" ] ; then
-        [ "$f" = ".git" ] && continue
-        [ "$f" = ".gitignore" ] && continue
-        [ "$f" = ".config" ] && continue
+    g=${f:2}
+    [ "$g" = "README.md" ] && continue
 
-        ln -snfv "$DOT_DIRECTORY/$f" "$HOME/$f"
-    fi
+    link_file "$DOT_DIR/$g" "$HOME/$g" "$BACKUP_DIR/$g"
 done
-if [ "$MODE" = "$force_mode" -o ! -e "${HOME}/.config/fish/config.fish" ] ; then
-    ln -snfv "$DOT_DIRECTORY/.config/fish/config.fish" "$HOME/.config/fish/config.fish"
-fi
 
-cd "$DOT_DIRECTORY/.config/git"
-for f in *
+cd $DOT_DIR__
+for f in `find -type f`
 do
-    if [ "$MODE" = "$force_mode" -o ! -e "${HOME}/.config/git/${f}" ] ; then
-    ln -snfv "$DOT_DIRECTORY/.config/git/$f" "$HOME/.config/git/$f"
+    g=${f:2}
+    if [ ! -e "$DOT_DIR/$g" ] ; then
+        link_file "$DOT_DIR__/$g" "$HOME/$g" "$BACKUP_DIR/$g"
     fi
 done
