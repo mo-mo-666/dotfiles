7fc6f3ab1233ce3e7c2a1ccefbe02a393a47470b
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2020-06-03 23:18:33 +0900

    add utility

---
+++ util/create_ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -----------------
+# Create ssh key
+# -----------------
+
+if [ ! -f "$HOME/.ssh/id_rsa.pub" ] \
+   && [ ! -f "$HOME/.ssh/id_dsa.pub" ]  \
+   && [ ! -f "$HOME/.ssh/id_ecdsa.pub" ] \
+   && [ ! -f "$HOME/.ssh/id_ed25519.pub" ] ; then
+    ssh-keygen -t rsa -b 4096 -C "mo-mo-666.pu_ki@outlook.com"
+fi
+
+cat "$HOME/.ssh/id_rsa.pub"
